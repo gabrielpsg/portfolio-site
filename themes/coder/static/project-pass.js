a1810943331adae094c5df7d29dc917d16ba2db6
Gabriel <gabriel_gomes@outlook.com>
2018-07-05 18:46:35 -0300

    test1

---
+++ themes/coder/static/project-pass.js
@@ -0,0 +1,12 @@
+
+        var password;
+        var pass1="love";
+
+        password=prompt('Password needed. Drop me an email if you want :] ');
+
+        if (password==pass1)
+          alert('Love is the key. Go ahead.');
+        else
+           {
+            window.location="/fleury.html";
+            }
